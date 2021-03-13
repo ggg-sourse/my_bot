2761d7be9555ec5b77e9179bb75d402c557ee463
ggg-sourse <80149120+ggg-sourse@users.noreply.github.com>
2021-03-13 11:31:45 +0200

    Update main_bot.py

--- main_bot.py
+++ main_bot.py
@@ -3,7 +3,7 @@ import configparser
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import Updater, CallbackQueryHandler, CommandHandler, MessageHandler, Filters
 import urllib.request
-LINK = 'https://ulkabo.github.io/bot-10-00/data/'
+LINK = ' https://ggg-sourse.github.io/my_bot/.'
 '''
 TOK = None
 config = configparser.ConfigParser()
@@ -186,7 +186,7 @@ def conditions(update, context):
 
     
 def main():
-    updater = Updater("1612051672:AAHr1oAA9dLdABLUJKgZ7mX0lgYj1cULjSg", use_context=True)
+    updater = Updater("1616090016:AAHk4mRXI_czN2V6MISBwCkx1Iygf4IX6m8", use_context=True)
 
     dp = updater.dispatcher
 
